9cb324a367a2581963e8130e47cc5795919baf0d
ahmed3520 <ataha352000@gmail.com>
2022-05-15 14:20:45 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState, useEffect} from 'react';
 function App() {
-  const [status, setStatus] = useState(true)
-  const [deviceTouchStatus, setDeviceTouchStatus] = useState(false)
+  var status=true
   const arr = [
     {
       name:"Furniture",
@@ -88,13 +86,10 @@ function App() {
   ]
   const top = arr.slice(0,11)
   const bottom = arr.slice(12, arr.length)
-  console.log(window.matchMedia("(pointer: coarse)").matches)
-  useEffect(()=>{
-    setDeviceTouchStatus(window.matchMedia("(pointer: coarse)").matches)
-  },[])
+
   return (
     <>
-    { deviceTouchStatus?
+
     <div className='cat-comp'>
       <div className='comp-list'>
         <div className='top-cat'>
@@ -115,7 +110,7 @@ function App() {
           {
             status?
           
-            <a className='cat-item' onClick={(e)=>setStatus(!status)}>
+            <a className='cat-item ' onClick={(e)=>(!status)}>
                   <div className="item-index">
                   <svg viewBox="0 0 72 72"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(0.111111, 0.000000)" stroke="#333B3F" stroke-width="1.5"><g transform="translate(8.000000, 28.666667)"><g><circle cx="7.33333333" cy="7.33333333" r="7.33333333"></circle><circle cx="28" cy="7.33333333" r="7.33333333"></circle><circle cx="48.6666667" cy="7.33333333" r="7.33333333"></circle></g></g></g></g></svg>
                     <span className='item-name'>More</span>
@@ -151,20 +146,20 @@ function App() {
           {
             !status?
             <>
-           <a className='cat-item' onClick={(e)=>setStatus(!status)}>
+           <a className='cat-item' onClick={(e)=>(!status)}>
            <i class="item-index" aria-hidden="true">
 
                 <svg viewBox="0 0 72 72">
                   <path fill="#eee" d="M52.8 29.421c0 8.793-7.287 15.921-16.275 15.921s-16.275-7.128-16.275-15.921c0-8.793 7.287-15.921 16.275-15.921s16.275 7.128 16.275 15.921z"></path><path fill="#eee" d="M7.582 54.587h56.837c1.702 0 3.082 1.38 3.082 3.082v0c0 1.702-1.38 3.082-3.082 3.082h-56.837c-1.702 0-3.082-1.38-3.082-3.082v0c0-1.702 1.38-3.082 3.082-3.082z"></path></svg></i></a>
-                  <a className='cat-item' onClick={(e)=>setStatus(!status)}>
+                  <a className='cat-item' onClick={(e)=>(!status)}>
                   <i class="item-index" aria-hidden="true">
 
                 <svg viewBox="0 0 72 72"><path fill="#eee" d="M52.8 29.421c0 8.793-7.287 15.921-16.275 15.921s-16.275-7.128-16.275-15.921c0-8.793 7.287-15.921 16.275-15.921s16.275 7.128 16.275 15.921z"></path><path fill="#eee" d="M7.582 54.587h56.837c1.702 0 3.082 1.38 3.082 3.082v0c0 1.702-1.38 3.082-3.082 3.082h-56.837c-1.702 0-3.082-1.38-3.082-3.082v0c0-1.702 1.38-3.082 3.082-3.082z"></path></svg></i></a>
-                <a className='cat-item' onClick={(e)=>setStatus(!status)}>
+                <a className='cat-item' onClick={(e)=>(!status)}>
                 <i class="item-index" aria-hidden="true">
 
                 <svg viewBox="0 0 72 72"><path fill="#eee" d="M52.8 29.421c0 8.793-7.287 15.921-16.275 15.921s-16.275-7.128-16.275-15.921c0-8.793 7.287-15.921 16.275-15.921s16.275 7.128 16.275 15.921z"></path><path fill="#eee" d="M7.582 54.587h56.837c1.702 0 3.082 1.38 3.082 3.082v0c0 1.702-1.38 3.082-3.082 3.082h-56.837c-1.702 0-3.082-1.38-3.082-3.082v0c0-1.702 1.38-3.082 3.082-3.082z"></path></svg></i></a>
-             <a className='cat-item' onClick={(e)=>setStatus(!status)}>
+             <a className='cat-item' onClick={(e)=>(!status)}>
               <i class="item-index" aria-hidden="true">
               <svg viewBox="0 0 72 72"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g stroke="#333B3F" stroke-width="1.32"><rect id="Rectangle-2" x="8" y="30" width="56" height="14" rx="7"></rect></g></g></svg>
                 
@@ -178,10 +173,7 @@ function App() {
         </div>
       </div>
     </div>
-    :<>
-    <p>Ping...Pong</p>
-    </>
-    }
+
     </>
   );
 }
